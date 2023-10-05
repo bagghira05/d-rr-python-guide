099797fbb7defa0a660b00777dcd4a8418ecd028
bagghira05 <87773307+bagghira05@users.noreply.github.com>
2023-10-05 19:16:54 +0200

    snake src and example code

--- settings.py
+++ settings.py
@@ -3,4 +3,6 @@ import pathlib
 
 
 BASE_DIR = pathlib.Path(__file__).parent
-SPRITE_DIR = BASE_DIR / "src" / "sprites"
+SPRITE_DIR = BASE_DIR / "src" / "imgs"
+SOUND_DIR = BASE_DIR / "src" / "sound"
+FONT_DIR = BASE_DIR / "src" / "font"
